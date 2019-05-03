7e735480b6f118ac76405ef8ecdead475a97f8f5
Alexandria Davis <amdavis7@dons.usfca.edu>
2019-05-02 22:11:39 -0700

    Prim's algorithm updated

--- src/algo/PrimAlgorithm.java
+++ src/algo/PrimAlgorithm.java
@@ -1,11 +1,13 @@
 package algo;
 
 import graph.*;
+import heap.PriorityQueue;
 
 /** Subclass of MSTAlgorithm. Uses Prim's algorithm to compute MST of the graph. */
 public class PrimAlgorithm extends MSTAlgorithm {
-    int[][] primTable; //= new int[numNodes][2];
+    private int[][] primTable;
     private int sourceVertex;
+    private int max= Integer.MAX_VALUE;
 
     /**
      * Constructor for PrimAlgorithm. Takes the graph
@@ -14,8 +16,30 @@ public class PrimAlgorithm extends MSTAlgorithm {
      */
     public PrimAlgorithm(Graph graph, int sourceVertex) {
         super(graph);
+        PriorityQueue newPriorityQueue= new PriorityQueue();
         this.sourceVertex = sourceVertex;
-        
+        int numNodes= super.numNodes();
+        primTable=new int[numNodes][2];
+        for (int x=0;x<numNodes;x++){
+            primTable[x][1]=max;
+            primTable[x][2]=-1;
+        }
+        primTable[sourceVertex][1]=0;
+        primTable[sourceVertex][2]=-1;
+        for(int i=0; i<numNodes; i++){
+            int v = newPriorityQueue.remove();
+            Edge head= super.getFirstEdge(v);
+
+            while(head!=null){
+                if (primTable[head.getId2()][2]==-1){
+                    if (primTable[head.getId2()][1]>head.getCost()){
+                        primTable[head.getId2()][1]=head.getCost();
+                        primTable[head.getId2()][2]=head.getId1();
+                    }
+                }
+            }
+        }
+
     }
 
     /**
@@ -26,6 +50,7 @@ public class PrimAlgorithm extends MSTAlgorithm {
     public void computeMST() {
         // FILL IN CODE
         // Note: must use a PriorityQueue and be efficient
+
 
 
 
