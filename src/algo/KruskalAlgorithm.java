09fbe2ea16fa36ad0e77a8a55fd4b3619855c2ea
Alexandria Davis <amdavis7@dons.usfca.edu>
2019-05-08 22:19:48 -0700

    Part 2- Kruskal's Complete

--- src/algo/KruskalAlgorithm.java
+++ src/algo/KruskalAlgorithm.java
@@ -1,16 +1,25 @@
 package algo;
 
 import graph.*;
+import sets.DisjointSets;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 /** Subclass of MSTAlgorithm. Computes MST of the graph using Kruskal's algorithm. */
 public class KruskalAlgorithm extends MSTAlgorithm {
-
+    private int numNodes;
+    //private Edge[] adjacencyList;
     /**
      * Constructor for KruskalAlgorithm. Takes the graph
      * @param graph input graph
      */
     public KruskalAlgorithm(Graph graph) {
         super(graph);
+        numNodes=super.numNodes();
+        //adjacencyList= new Edge[numNodes];
     }
 
     /**
@@ -20,6 +29,26 @@ public class KruskalAlgorithm extends MSTAlgorithm {
     @Override
     public void computeMST() {
         // FILL IN CODE
+        DisjointSets sets= new DisjointSets(numNodes);
+        sets.createSets(numNodes);
+        ArrayList<Edge> Edges=new ArrayList<Edge>();
+        for(int i=0;i<numNodes;i++){
+            Edge head= super.getFirstEdge(i);
+            while (head!=null){
+                Edges.add(head);
+                head=head.next();
+            }
+
+        }
+        Collections.sort(Edges);
+        for (int z=0;z<Edges.size();z++){
+            int x=Edges.get(z).getId1();
+            int y=Edges.get(z).getId2();
+            if(sets.find(x)!=sets.find(y)){
+                addMSTEdge(Edges.get(z));
+                sets.union(x,y);
+            }
+        }
 
     }
 
--- src/sets/DisjointSets.java
+++ src/sets/DisjointSets.java
@@ -7,8 +7,15 @@ package sets;
 public class DisjointSets {
     private int[] parent;
 
+    public DisjointSets(int x){
+        parent= new int[x];
+        createSets(x);
+    }
     public void createSets(int n) {
         // FILL IN CODE
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
     }
 
     /**
@@ -19,8 +26,10 @@ public class DisjointSets {
      */
     public int find(int x) {
         // FILL IN CODE
-
-        return -1; // change
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x]; // change
     }
 
     /**
@@ -30,6 +39,26 @@ public class DisjointSets {
      */
     public void union(int x, int y) {
         // FILL IN CODE
+        int xRoot = find(x), yRoot = find(y);
+        if (xRoot == yRoot){
+            return;
+        }
+        if (parent[xRoot] < parent[yRoot]){
+            parent[xRoot] = yRoot;
+        }
+        else if (parent[yRoot] < parent[xRoot]){
+            parent[yRoot] = xRoot;
+        }
+        else{
+            parent[yRoot] = xRoot;
+        }
+    }
+
+    public static void main(String[] args) {
+        DisjointSets set= new DisjointSets(5);
+        set.union(1,3);
+        set.union(3,4);
+        System.out.println(set.find(1));
 
     }
 
